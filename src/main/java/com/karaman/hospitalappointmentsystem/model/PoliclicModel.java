bb32afa127d33b4d7d31ffb03e0e4dfdf79b46e1
Karani <karamankarani42@gmail.com>
2022-04-21 23:25:33 +0300

    policlinic model added

--- src/main/java/com/karaman/hospitalappointmentsystem/model/PoliclicModel.java
+++ src/main/java/com/karaman/hospitalappointmentsystem/model/PoliclicModel.java
@@ -2,11 +2,21 @@ package com.karaman.hospitalappointmentsystem.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name="Policlic")
 public class PoliclicModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="policlicId")
+    private Long policlicId;
+
+    @Column(name="policlic_name")
+    private String policlicName;
+
+
+
 }
